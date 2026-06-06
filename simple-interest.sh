dc153ecdab9db29b39989f51acc4b0170b6a6d32
pyegrave <dennis.freter@gmail.com>
2026-06-06 13:44:16 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
 
-   # Do not use this in production. Sample purpose only.
+# Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# <your GitHub username>
 
-   # Input:
-   # p, principal amount
-   # t, time period in years
-   # r, annual rate of interest
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
 
-   # Output:
-   # simple interest = p*t*r
+# Output:
+# simple interest = p*t*r
 
    echo "Enter the principal:"
    read p
